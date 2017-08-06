86474113a7e3b63bfc2e6ece88b8eb3142ba2b01
SimonM <podvilla@gmail.com>
2017-08-06 12:06:17 +0100

    Updates, added logo

--- go.bat
+++
@@ -1,8 +0,0 @@
-cd images
-
-for %%x in (*.*) do ..\pngquant -f --speed 1 --nofs --posterize 4 --output "quant\%%x" 16 "%%x"
-rem pngquant --output output\images\quant\*.png output\images\*.png
-
-
-
-pause
---
+++ output/logo/config.asm
@@ -0,0 +1,1 @@
+PUTFILE "output/logo/nula.png.bbc.exo", "A.01", &74f0, &2fc0
