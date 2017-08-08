ce8ad1a00dc55dda574429c2556eb09425285689
SimonM <podvilla@gmail.com>
2017-08-08 20:03:45 +0100

    Refactored compilation script to support per-image options

--- output/wip/config.asm
+++ output/wip/config.asm
@@ -1,6 +1,6 @@
-PUTFILE "output/wip/crossy.png.bbc.exo", "A.01", &6f27, &2fc0
-PUTFILE "output/wip/parrots.png.bbc.exo", "A.02", &69e3, &2fc0
-PUTFILE "output/wip/populous.png.bbc.exo", "A.03", &6a0a, &2fc0
-PUTFILE "output/wip/teapot.png.bbc.exo", "A.04", &7681, &2fc0
-PUTFILE "output/wip/test.png.bbc.exo", "A.05", &7f1d, &2fc0
-PUTFILE "output/wip/tut2.png.bbc.exo", "A.06", &5939, &2fc0
+PUTFILE "output/wip/crossy.png.bbc.exo", "A.01", &6f22, &2fc0
+PUTFILE "output/wip/parrots.png.bbc.exo", "A.02", &69e1, &2fc0
+PUTFILE "output/wip/populous.png.bbc.exo", "A.03", &6a07, &2fc0
+PUTFILE "output/wip/teapot.png.bbc.exo", "A.04", &7680, &2fc0
+PUTFILE "output/wip/test.png.bbc.exo", "A.05", &7f17, &2fc0
+PUTFILE "output/wip/tut2.png.bbc.exo", "A.06", &593b, &2fc0
